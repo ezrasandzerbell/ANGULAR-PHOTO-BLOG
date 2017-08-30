518645a4fa0df04bd749e4c0fcf34d8d4ef8d536
ezra sandzer-bell <equilibriumarts@gmail.com>
2017-08-29 20:17:13 -0700

    post data to backend using a service

--- src/app/entries/entry/entry-comment-form.component.ts
+++ src/app/entries/entry/entry-comment-form.component.ts
@@ -1,8 +1,9 @@
 // Event Emitter makes it possible to pass data between components
 // Output decorator allows sending data out to the parent, via the event emitter
-
-import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
+// Input decorator allows passage of data from Entry component to EntryCommentForm component
+import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { EntryService } from '../shared/entry.service';
 
 
 @Component ({
@@ -16,16 +17,29 @@ export class EntryCommentFormComponent {
     name: string = "";
     comment: string = "";
 
+    // make entryId available to the component
+    @Input() entryId: number;
+
     // this @Output decorator is the piece that sends the emitted event to the browser
     @Output() onCommentAdded = new EventEmitter<{name: string; comment: string;}>();
 
-    // @ViewChild decorator is bound to the local variable 'commentForm' from the #commentForm variable as seen in the html template.
+    // @ViewChild decorator is bound to the local variable 'commentForm' from template's #commentForm variable
     // commentForm (no quotes), now becomes a member of this EntryCommentForm component.
     @ViewChild('commentForm') commentForm: NgForm;
+
+    // constructor pulls entryService into the class and makes it available to the onSubmit method
+    constructor(private entryService: EntryService){
+
+    }
 
     // commentForm variable passed through onSubmit is an instance of NgForm (imported above)
     onSubmit(commentForm: NgForm) {
         let comment = { name: this.name, comment: this.comment };
+        this.entryService.addComment(this.entryId, comment)
+            .then(() => {
+                this.onCommentAdded.emit(comment);
+                    this.commentForm.resetForm();
+            })
         this.onCommentAdded.emit(comment);
 
         // reset form method can be accessed due to NgForm argument
